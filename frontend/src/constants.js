d0bdedd9c95748a1e00a267261cb96ce303af68d
anish0905 <anishanish0102@gmail.com>
2024-06-18 13:25:35 +0530

    ok

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5003";
+export const BASE_URL = "http://localhost:5003"; //ok
 export const emplyeRegistration = `${BASE_URL}/api/employeeRegistration/register`;
 // updated
